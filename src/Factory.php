d0f8dd904b07778bdbb1c3d4e60c5ad094c7dc0c
kiranguragai <kiran_guragai@yahoo.com>
2014-06-30 14:47:14 +0545

    commit

---
+++ src/Factory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Runa\Demo;
+
+class Factory {
+
+	public function build() {
+		echo "This is demo";
+	}
+}
---
+++ tests/FactoryTest.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Runa\Demo\Factory;
+
+$factory = new Runa\Demo\Factory();
+$factory->build();
